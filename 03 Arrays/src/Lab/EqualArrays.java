d40b82c57631bd99d394f89a3f1a21a08a3c075f
Todor Kostov <todpavkos@gmail.com>
2020-11-09 13:31:00 +0100

    Una actividad

--- 03 Arrays/src/Lab/EqualArrays.java
+++ 03 Arrays/src/Lab/EqualArrays.java
@@ -1,5 +1,31 @@
 package Lab;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EqualArrays {
-     
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] firstArr = Arrays.stream(scan.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] secondArr = Arrays.stream(scan.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+
+        int maxLength = Math.max(firstArr.length, secondArr.length);
+        int sum = 0;
+        for (int i = 0; i < maxLength; i++) {
+            sum += firstArr[i];
+            if (firstArr[i] != secondArr[i]) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                break;
+            }
+            if (maxLength - 1 == i) {
+                System.out.printf("Arrays are identical. Sum: %d", sum);
+
+            }
+        }
+    }
+
 }
